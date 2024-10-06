314605e7022d3135a2d8fcd86025506466143f87
Bbest1365 <best1365@gmail.com>
2024-10-06 18:32:24 +0700

    update

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +26,51 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+a = input('What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n')
+b = random.randint(0, 2)
+
+if a == '0':
+    print(rock)
+    if b == 2:
+        print('Computer chose:')
+        print(scissors)
+        print('You win!')
+    elif b == int(a):
+        print('Computer chose:')
+        print(rock)
+        print("It's a draw")
+    else:
+        print('You lose!')
+
+elif a == '1':
+
+    print(paper)
+    if b == 0:
+        print('Computer chose:')
+        print(rock)
+        print('You win!')
+    elif b == int(a):
+        print('Computer chose:')
+        print(paper)
+        print("It's a draw")
+    else:
+        print('You lose!')
+
+elif a == '2':
+
+    print(scissors)
+    if b == 1:
+        print('Computer chose:')
+        print(paper)
+        print('You win!')
+    elif b == int(a):
+        print('Computer chose:')
+        print(scissors)
+        print("It's a draw")
+    else:
+        print('You lose!')
+
+
+else:
+        print('You typed invalid number, you lose!')
