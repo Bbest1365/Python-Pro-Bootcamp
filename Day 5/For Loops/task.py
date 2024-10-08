faba7adb9974215392f0074634736bc1da2792d8
Bbest1365 <best1365@gmail.com>
2024-10-08 20:10:32 +0700

    UPDATE

--- Day 5/For Loops/task.py
+++ Day 5/For Loops/task.py
@@ -1,1 +1,10 @@
 fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print("Hello")
+
+
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+print("Hello")
