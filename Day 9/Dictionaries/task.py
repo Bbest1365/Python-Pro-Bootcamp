813cc65a661d3365a8de50dc3e3fc5e5900f0f5e
Bbest1365 <best1365@gmail.com>
2024-10-14 00:12:03 +0700

    UPDATE

--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,8 @@
 programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+
+
+
+my_empty_dictionary = {}
+programming_dictionary["Today"] = "ishappy"
+programming_dictionary['Function'] = 'news'
+print(programming_dictionary)
