01033245566b0e9851c10738c4ba1596ab639fd1
best1365 <best1365@gmail.com>
2024-10-19 23:39:03 +0700

    update

--- Day 12/Number Guessing Project/main.py
+++ Day 12/Number Guessing Project/main.py
@@ -5,28 +5,51 @@ print('Welcome to the Number Guessing Game!')
 print("I'm thinking of a number between 1 and 100.")
 Type = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
 correct_number = random.randint(1,100)
-def select_type(Type):
+n1 = 10
+n2 = 5
+is_gameover = True
+
+
+while is_gameover:
     if Type == 'easy':
-        n = 10
-        print('You have 10 attempts remaining to guess the number.')
-        guess_number = input('Make a guess: ')
+        print(f'You have {n1} attempts remaining to guess the number.')
+        guess_number = int(input('Make a guess: '))
+        n1 -= 1
+        if guess_number == correct_number:
+            print(f'You got it! The answer was {correct_number}.')
+            is_gameover = False
+        elif guess_number < correct_number:
+            print('Too low.')
+            if n1 == 0:
+                print('You\'ve run out of guesses, you lose.')
+                is_gameover = False
+            print('Guess again.')
+
+        elif guess_number > correct_number:
+            print('Too high.')
+            if n1 == 0:
+                print('You\'ve run out of guesses, you lose.')
+                is_gameover = False
+            print('Guess again.')
 
     elif Type == 'hard':
-        n = 5
-        print('You have 5 attempts remaining to guess the number.')
-        input('Make a guess: ')
+        print(f'You have {n2} attempts remaining to guess the number.')
+        guess_number = int(input('Make a guess: '))
+        n2 -= 1
+        if guess_number == correct_number:
+            print(f'You got it! The answer was {correct_number}.')
+            is_gameover = False
+        elif guess_number < correct_number:
+            print('Too low.')
+            if n2 == 0:
+                print('You\'ve run out of guesses, you lose.')
+                is_gameover = False
+            print('Guess again.')
 
- def check()
-    if guess_number == correct_number:
-        print()
-    elif guess_number < correct_number:
-        print('Too low.')
-        print('Guess again.')
-        select_type(guess_number)
-    elif guess_number > correct_number:
-        print('Too high.')
-        print('Guess again.')
-        print(f'You have {n - 1} attempts remaining to guess the number.')
-        guess_number = input('Make a guess: ')
+        elif guess_number > correct_number:
+            print('Too high.')
+            if n2 == 0:
+                print('You\'ve run out of guesses, you lose.')
+                is_gameover = False
+            print('Guess again.')
 
-print(number)
