0585523800b163eab8996d7e8e8ca4d61d73dd6a
best1365 <best1365@gmail.com>
2024-10-23 00:33:59 +0700

    update

--- Day 14/Higher or Lower Project/main.py
+++ Day 14/Higher or Lower Project/main.py
@@ -1,9 +1,29 @@
 from art import logo, vs
 from game_data import data
 import random
-
+score = 0
 print(logo)
-print('Compare A: Instagram, a Social media platform, from United States.')
-print('Against B: Justin Timberlake, a Musician and actor, from United States.')
-followers = input("Who has more followers? Type 'A' or 'B': ")
+def rand():
+    compare_A = random.choice(data)
+# print(f'{compare_A['name']}, {compare_A['description']}, {compare_A['country']}')
+    against_B = random.choice(data)
+# print(f'{against_B['name']}, {against_B['description']}, {against_B['country']}')
+    print(f'Compare A: {compare_A['name']}, {compare_A['description']}, {compare_A['country']}')
+    print(vs)
+    print(f'Against B: {against_B['name']}, {against_B['description']}, {against_B['country']}')
+    return compare_A, against_B
+loop = True
+while loop:
+    compare_A, against_B = rand()
+    followers = input("Who has more followers? Type 'A' or 'B': ").upper()
+
+    if compare_A['follower_count'] > against_B['follower_count'] and followers == 'A':
+        score += 1
+        print(f"You're right! Current score: {score}.")
+    elif compare_A['follower_count'] < against_B['follower_count'] and followers == 'B':
+        score += 1
+        print(f"You're right! Current score: {score}.")
+    else:
+        print(f"Sorry, that's wrong. Final score: {score}")
+        loop = False
 
