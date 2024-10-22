b22b102320a137c8abfc5d5cd5f3a6ae384948e3
best1365 <best1365@gmail.com>
2024-10-22 23:56:10 +0700

    update

---
+++ Day 14/Higher or Lower Project/main.py
@@ -0,0 +1,9 @@
+from art import logo, vs
+from game_data import data
+import random
+
+print(logo)
+print('Compare A: Instagram, a Social media platform, from United States.')
+print('Against B: Justin Timberlake, a Musician and actor, from United States.')
+followers = input("Who has more followers? Type 'A' or 'B': ")
+
