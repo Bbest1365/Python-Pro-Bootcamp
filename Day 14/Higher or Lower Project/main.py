7ea8f26d4dfa3e90c0f84388e84e639ace383579
best1365 <best1365@gmail.com>
2024-10-23 21:40:31 +0700

    update

--- Day 14/Higher or Lower Project/main.py
+++ Day 14/Higher or Lower Project/main.py
@@ -3,14 +3,13 @@ from game_data import data
 import random
 score = 0
 print(logo)
+compare_A = random.choice(data)
 def rand():
-    compare_A = random.choice(data)
-# print(f'{compare_A['name']}, {compare_A['description']}, {compare_A['country']}')
     against_B = random.choice(data)
-# print(f'{against_B['name']}, {against_B['description']}, {against_B['country']}')
     print(f'Compare A: {compare_A['name']}, {compare_A['description']}, {compare_A['country']}')
     print(vs)
     print(f'Against B: {against_B['name']}, {against_B['description']}, {against_B['country']}')
+
     return compare_A, against_B
 loop = True
 while loop:
@@ -19,9 +18,11 @@ while loop:
 
     if compare_A['follower_count'] > against_B['follower_count'] and followers == 'A':
         score += 1
+        compare_A = against_B
         print(f"You're right! Current score: {score}.")
     elif compare_A['follower_count'] < against_B['follower_count'] and followers == 'B':
         score += 1
+        compare_A = against_B
         print(f"You're right! Current score: {score}.")
     else:
         print(f"Sorry, that's wrong. Final score: {score}")
