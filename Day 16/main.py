a771a1b0d63b48e52cfca3448e0dec7a5aadf041
best1365 <best1365@gmail.com>
2024-10-26 21:27:03 +0700

    start new OOP

--- Day 16/main.py
+++ Day 16/main.py
@@ -1,1 +1,16 @@
-'update'
+# import another_module
+# from turtle import Turtle, Screen
+#
+#
+# print(another_module.another_variable)
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape('turtle')
+# timmy.color("Orangered1")
+# timmy.forward(15)
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+import prettytable
+print(prettytable)
