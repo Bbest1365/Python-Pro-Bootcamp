f68c9142c7f990745435b34c223fc625dab71ff6
best1365 <best1365@gmail.com>
2024-10-27 10:09:24 +0700

    update

--- Day 16/main.py
+++ Day 16/main.py
@@ -16,5 +16,6 @@ from prettytable import PrettyTable
 table = PrettyTable()
 table.add_column("Pokemon Name",["Pikachu","Squirtie","Charmander"])
 table.add_column("Type",["Electric", "Water", "Fire"])
-
+table.align = 'l'
+print(table.align)
 print(table)
