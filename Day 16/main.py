dd9aac35b3da4ab59bbd5d3cc23097fc33fa6521
best1365 <best1365@gmail.com>
2024-10-27 10:05:58 +0700

    start new OOP

--- Day 16/main.py
+++ Day 16/main.py
@@ -12,5 +12,9 @@
 # print(my_screen.canvheight)
 # my_screen.exitonclick()
 
-import prettytable
-print(prettytable)
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name",["Pikachu","Squirtie","Charmander"])
+table.add_column("Type",["Electric", "Water", "Fire"])
+
+print(table)
