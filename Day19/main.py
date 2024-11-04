9bea7e4fc54404529e40aefa462e044304af30eb
Best1365 <best1365@gmail.com>
2024-11-05 01:12:55 +0700

    update number of turtle max is 34 depense on number of color that create! good job!

--- Day19/main.py
+++ Day19/main.py
@@ -3,23 +3,33 @@ import random
 screen = Screen()
 screen.screensize(500,400)
 user_bet = screen.textinput(title = "Make your bet", prompt="Which turtle will win the race ? Enter a color: ")
-colors = ["red",'blue','green','yellow','black','purple']
-tao = ['tao1','tao2','tao3','tao4','tao5','tao6']
-j = 200
-for index in range(0,6):
-    tao[index] = Turtle()
-    tao[index].shape('turtle')
-    tao[index].shapesize(2)
-    tao[index].color(colors[index])
-    tao[index].penup()
-    tao[index].goto(-500,j)
+colors = [
+    "red", "blue", "green", "yellow", "black", "purple",
+    "gold", "grey", "pink", "cyan", "magenta", "orange",
+    "brown", "teal", "navy", "lavender", "coral", "turquoise",
+    "salmon", "beige", "lime", "indigo", "violet", "peach",
+    "chocolate", "crimson", "maroon", "mint", "khaki",
+    "plum", "orchid", "tan", "auburn", "sapphire"
+]
+#max number_of_turtle_is 34
+
+All_taos = []
+j = 400
+for index in range(0,9):
+    tao = Turtle()
+    tao.shape('turtle')
+    tao.shapesize(2)
+    tao.color(colors[index])
+    tao.penup()
+    tao.goto(-500,j)
+    All_taos.append(tao)
     j -= 100
 
 if user_bet:
     is_race_on = True
 while is_race_on:
-    for tao_name in tao:
-        if tao_name.xcor() > 480:
+    for tao_name in All_taos:
+        if tao_name.xcor() > 400:
             is_race_on = False
             winning_color = tao_name.pencolor()
             if user_bet == winning_color:
