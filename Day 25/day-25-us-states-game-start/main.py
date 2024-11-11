ba837a8dfe939f96f79ffe3829911a021b977ac1
Best1365 <best1365@gmail.com>
2024-11-12 01:13:38 +0700

    update pandas U.S. States Game

--- Day 25/day-25-us-states-game-start/main.py
+++ Day 25/day-25-us-states-game-start/main.py
@@ -9,20 +9,37 @@ screen.title("U.S States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
-while True:
-    answer_state = screen.textinput(title = "Guess the State", prompt="What's another state's name?")
 
-    data = pd.read_csv('50_states.csv')
+data = pd.read_csv('50_states.csv')
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title = f"{len(guessed_states)}/50 Status Correct",
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pd.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+
+        break
 
     for state in data['state']:
-        if  state.lower() == answer_state.lower():
+        if  state == answer_state:
             true_state = data[data['state'] == state]
-            x = true_state['x']
-            y = true_state['y']
+            x = true_state['x'].item()
+            y = true_state['y'].item()
             my_turtle.penup()
             my_turtle.hideturtle()
-            my_turtle.goto(int(x),int(y))
-            my_turtle.write(state,align="center",font=("Arial",16,"normal"))
+            my_turtle.goto(x,y)
+            my_turtle.write(state)
+            guessed_states.append(state)
+
+# States to learn.csv
+
 
 
 
