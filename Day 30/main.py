5ce382890d93fd848bbc4f28f47d62c2896bb247
Best1365 <best1365@gmail.com>
2024-11-22 23:55:39 +0700

    Update

--- Day 30/main.py
+++ Day 30/main.py
@@ -2,6 +2,16 @@
 
 try:
     file = open("a_file.txt")
-except:
+    a_dictionary = { "key":"value"}
+    print(a_dictionary["sdfsdf"])
+except FileNotFoundError:
     file = open("a_file.txt", "w")
-
+    file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File was closed.")
