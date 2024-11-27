ca7941cbb011c05bf961071c0721918af93f1c85
Best1365 <best1365@gmail.com>
2024-11-27 23:59:54 +0700

    Update

--- Day 31/flash-card-project-start/main.py
+++ Day 31/flash-card-project-start/main.py
@@ -1,7 +1,12 @@
 from tkinter import *
+import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict()
+print(to_learn)
 def next_card():
+    pass
 
 
 
@@ -14,7 +19,7 @@ canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
 canvas.create_text(400, 150, text="French", font=("Arial",40, 'italic'))
-with open('/data/french_words.csv','r') as data_file:
+# with open('/data/french_words.csv','r') as data_file:
 
 canvas.create_text(400, 263, text="word", font=('Ariel', 60,'bold'))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
