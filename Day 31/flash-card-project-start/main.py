b2084bacd8901a1cb6ff2c1467d5b6db18a43ae4
Bbest1365 <best1365@gmail.com>
2024-11-28 23:22:26 +0700

    update

--- Day 31/flash-card-project-start/main.py
+++ Day 31/flash-card-project-start/main.py
@@ -1,13 +1,15 @@
 from tkinter import *
 import pandas
 import random
+
 BACKGROUND_COLOR = "#B1DDC6"
+
 data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict()
+to_learn = data.to_dict(orient="records")
+
 print(to_learn)
 def next_card():
     current_card = random.choice(to_learn)
-    print(current_card["French"])
     canvas.itemconfig(card_title, text="French")
     canvas.itemconfig(card_word, text=current_card["French"])
 
@@ -18,10 +20,8 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="French", font=("Arial",40, 'italic'))
-# with open('/data/french_words.csv','r') as data_file:
-
-canvas.create_text(400, 263, text="word", font=('Ariel', 60,'bold'))
+card_title = canvas.create_text(400, 150, text="", font=("Arial",40, 'italic'))
+card_word =canvas.create_text(400, 263, text="", font=('Ariel', 60,'bold'))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
