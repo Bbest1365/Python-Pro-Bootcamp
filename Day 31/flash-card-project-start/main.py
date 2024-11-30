43afb7d44a5f112d8f60d3a40d65fb19ae541d7b
Bbest1365 <best1365@gmail.com>
2024-11-30 23:29:44 +0700

    update

--- Day 31/flash-card-project-start/main.py
+++ Day 31/flash-card-project-start/main.py
@@ -15,8 +15,9 @@ def next_card():
     canvas.itemconfig(card_word, text=current_card["French"])
 
 def flip_card():
-    canvas.itemconfig(card_title, text="English")
-    canvas.itemconfig(card_word, text=current_card["English"])
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
@@ -25,7 +26,8 @@ window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file="images/card_back.png")
 card_title = canvas.create_text(400, 150, text="", font=("Arial",40, 'italic'))
 card_word =canvas.create_text(400, 263, text="", font=('Ariel', 60,'bold'))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
